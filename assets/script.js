09cad39f33fc2a0556d9a610806dfecf43f40f9f
bechedley <109792989+bechedley@users.noreply.github.com>
2022-09-01 22:34:58 +1000

    Added password length prompt window and error message

--- assets/script.js
+++ assets/script.js
@@ -8,6 +8,31 @@ function writePassword() {
 
   passwordText.value = password;
 
+  // Add function message for password length error
+
+  function lengthErrorMessage() {
+    window.alert('Sorry, your password must be between 8 and 128 characters. Please try again.')
+  }
+
+  // Add prompt window for password length
+
+  var passwordLength = window.prompt('How many characters does your password need to be?');
+
+  // If user pressed Cancel, immediately end function
+  if (!passwordLength) {
+    return;
+  }
+
+  // If user chose a value less than 8, display re-try message
+  else if (
+    (passwordLength < 8) ||
+    (passwordLength > 128)
+  ) {
+    lengthErrorMessage();
+  }
+
+
+
 }
 
 // Add event listener to generate button
